7201bda8be3b8a55b2dc9b388d456dc4bace9b8f
Jose Guilherme Nascimento Vieira da Silva <jgnvs@WINDOWS.CIN.UFPE.BR>
2018-06-21 11:25:37 -0300

    implementacao das opcoes de empresa na interface de usuario.

--- ProjetoIP-VEM/src/InterfaceUsuario.java
+++ ProjetoIP-VEM/src/InterfaceUsuario.java
@@ -55,9 +55,6 @@ public class InterfaceUsuario {
 					}
 				}
 				
-				
-				
-			
 			}else if(op.equals("2")) {
 				boolean sairEmpresa = false;
 				String opEmpresa; 
@@ -68,15 +65,85 @@ public class InterfaceUsuario {
 					System.out.println("3 - Excluir Empresa");
 					System.out.println("4 - Sair");
 					System.out.println("--------------------------------------------------------------------------------------");
-					opEmpresa = in.next();
+					opEmpresa = in.nextLine();
+					
 					if(opEmpresa.equals("1")) {
-						//implentando
+						System.out.println("Insira o nome da empresa:");
+						String nome = in.nextLine();
+						System.out.println("Insira o cnpj da empresa:");
+						String cnpj = in.nextLine();
+						System.out.println("Insira o endreço:");
+						String endereco = in.nextLine();
+						System.out.println("Insira o telefone:");
+						String telefone = in.nextLine();
+						empresa = new EmpresaVem(nome,cnpj,endereco,telefone);
+						sistema.cadastrarEmpresa(empresa);
+					
+						
 					}else if(opEmpresa.equals("2")) {
-						//implentando
+						System.out.println("Insira o cnpj da empresa que você deseja alterar:");
+						System.out.println("(Insira 0 caso deseje voltar:)");
+						String cnpj =  in.nextLine();
+						while(!cnpj.equals("0")) {
+							if(sistema.existeEmpresa(cnpj)) {
+								empresa = sistema.procurarEmpresa(cnpj);
+								String opAtualizarVem;
+								System.out.println("--------------------------------------------------------------------------------------");
+								System.out.println("1 - Alterar nome");			
+								System.out.println("2 - Alterar endereço");			
+								System.out.println("3 - Alterar telefone");
+								System.out.println("--------------------------------------------------------------------------------------");
+								opAtualizarVem = in.nextLine();
+								if(opAtualizarVem.equals("1") || opAtualizarVem.equals("2") || opAtualizarVem.equals("3")) {
+									if(opAtualizarVem.equals("1")) {
+										System.out.println("Insira o novo nome da empresa:");
+										empresa.setNomeEmpresa(in.nextLine());
+									}else if(opAtualizarVem.equals("2")) {
+										System.out.println("Insira o novo endereço da empresa:");
+										empresa.setEndereco(in.nextLine());
+									}else {
+										System.out.println("Insira o novo telefone da empresa:");
+										empresa.setFone(in.nextLine());
+									}
+									System.out.println("Alteração realizada com sucesso!!!");
+									cnpj = "0";
+								}else {
+									System.out.println("ERRO!!!");
+									System.out.println("Você não inseriu nenhuma das opções validas!");
+								}
+							}else {
+								System.out.println("ERRO!!!");
+								System.out.println("empresa com esse cnpj não foi encontrada!");
+								System.out.println("Insira o cnpj novamente:");
+								System.out.println("(Insira 0 caso deseje voltar:)");
+								cnpj =  in.nextLine();
+							}
+						}
+						
+						
 					}else if(opEmpresa.equals("3")) {
-						//implentando
+						System.out.println("Insira o cnpj da empresa que você deseja excluir:");
+						System.out.println("(Insira 0 caso deseje voltar)");
+						String cnpj =  in.nextLine();
+						while(!cnpj.equals("0")) {
+							if(sistema.existeEmpresa(cnpj)) {
+								sistema.removerEmpresa(cnpj);
+								System.out.println("Empresa excluida com sucesso!!!");
+								cnpj = "0";
+							}
+							else {
+								System.out.println("ERRO!!!");
+								System.out.println("empresa com esse cnpj não foi encontrada!");
+								System.out.println("(Insira 0 caso deseje voltar)");
+								System.out.println("Insira o cnpj novamente:");
+								cnpj =  in.nextLine();
+							}
+						}
+						
 					}else if(opEmpresa.equals("4")) {
 						sairEmpresa = true;
+						
+						
 					}else {
 						System.out.println("Entrada invalida!!!");
 					}
